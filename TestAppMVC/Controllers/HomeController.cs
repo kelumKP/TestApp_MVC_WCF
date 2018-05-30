fa4e61a51ce357067b9315d5068138fbc49895c4
Kelum Priyadarshana <kelumkanthaka@hotmail.com>
2018-05-30 16:59:33 +0800

    Add project files.

---
+++ TestAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestAppMVC.Models;
+
+
+namespace TestAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GameDetails()
+        {
+            ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();
+            return View(obj.GetGameDetails());
+        }
+    }
+}
---
+++ TestAppWCF/IService1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TestAppWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        List<GameTable> GetGameDetails();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class GameDetails
+    {
+        
+        string playerA = string.Empty;
+        string playerB = string.Empty;
+        string result = string.Empty;
+
+        [DataMember]
+        public string PlayerA
+        {
+            get { return playerA; }
+            set { playerA = value; }
+        }
+
+        [DataMember]
+        public string PlayerB
+        {
+            get { return playerB; }
+            set { playerA = value; }
+        }
+
+        [DataMember]
+        public string Result
+        {
+            get { return Result; }
+            set { result = value; }
+        }
+
+
+    }
+}
